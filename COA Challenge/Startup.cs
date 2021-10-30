4c6c254ecafa41a865253eef3ec4c0919ebfa310
Alejandro Jacobo <thesenntinel@outlook.com>
2021-10-30 16:11:42 -0300

    Se agrega EntityCore al projecto, y SQL

--- COA Challenge/Startup.cs
+++ COA Challenge/Startup.cs
@@ -26,6 +26,8 @@ namespace COA_Challenge
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddEntityFrameworkSqlServer();
+            
 
             services.AddControllers();
             services.AddSwaggerGen(c =>
