d66f9fda7e9978da32281c9c5dd9a7f085b4ee1c
Alejandro Jacobo <thesenntinel@outlook.com>
2021-10-30 19:28:55 -0300

    Se agrega Unit Of Work. Implementara automaticamente un repositorio en el cual se trabajara, dependiendo de la entidad. Se tiene que instanciar como un IBaseRepository
    Se agregan metodos de Save, SAync y Dispose

--- COA.Infrastructure/Interfaces/IUnitOfWork.cs
+++ COA.Infrastructure/Interfaces/IUnitOfWork.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using COA.Domain;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +9,10 @@ namespace COA.Infrastructure.Repositories.Interfaces
 {
     public interface IUnitOfWork
     {
+        IBaseRepository<User> UsersRepository { get; }
 
+        void Dispose();
+        void SaveChanges();
+        Task SaveChangesAsync();
     }
 }
