cc53eb1d7b3c70ca7e215138690b3c28757a2e57
Alejandro Jacobo <thesenntinel@outlook.com>
2021-10-30 16:25:13 -0300

    Se incorpora Repositorio Base y Unidad de Trabajo. Se crean Interfases de las mismas. Se trabajara con una EntityBase, por ahora queda como Generic: T:class

---
+++ COA.Infrastructure/Repositories/BaseRepository.cs
@@ -0,0 +1,13 @@
+﻿using COA.Infrastructure.Repositories.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace COA.Infrastructure.Repositories
+{
+    public class BaseRepository<T> : IBaseRepository<T> where T : class
+    {
+    }
+}
---
+++ COA.Infrastructure/Repositories/Interfaces/IBaseRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace COA.Infrastructure.Repositories.Interfaces
+{
+    interface IBaseRepository<T> where T : class
+    {
+        
+
+    }
+}
