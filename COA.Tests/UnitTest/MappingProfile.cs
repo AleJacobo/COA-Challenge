0e6b13fa8b5addb31c38a53b0e752468c567091e
Alejandro Jacobo <thesenntinel@outlook.com>
2021-11-01 02:45:51 -0300

    Se agregan Unit Test a la API. Se verifican par de incongruencias en las rutas de los controllers, en cuestion de los ActionResult.
    Se corrigen acorde.
    
    Se agregan 10 tests, 2 por ruta con un resultado Ok y uno Bad
    
    Funcionando correctamente!

---
+++ COA.Tests/UnitTest/MappingProfile.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using COA.Domain;
+using COA.Domain.DTOs.UserDTOs;
+
+namespace COA.Tests.Tests
+{
+    internal class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<User, UserDTO>().ReverseMap();
+            CreateMap<User, UserInsertDTO>().ReverseMap();
+            CreateMap<User, UserUpdateDTO>().ReverseMap();
+        }
+    }
+}
