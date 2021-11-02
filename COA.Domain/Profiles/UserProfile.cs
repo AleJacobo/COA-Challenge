3f78375b0f2d4cd2451b6e3a806ea1029a3bccf6
Alejandro Jacobo <thesenntinel@outlook.com>
2021-11-02 03:13:01 -0300

    Se agrega documentacion con SwashBuckle, se detallan los endpoints, las clases custom y la clase Result

--- COA.Domain/Profiles/UserProfile.cs
+++ COA.Domain/Profiles/UserProfile.cs
@@ -3,6 +3,9 @@ using COA.Domain.DTOs.UserDTOs;
 
 namespace COA.Domain.Profiles
 {
+    /// <summary>
+    /// Perfil de User de Automapper
+    /// </summary>
     public class UserProfile : Profile
     {
         public UserProfile()
